ef642e4f1ccb5f60655d99cd40bd76078e62a9ea
Gwapobenjie <plazabenjie1@gmail.com>
2025-05-16 03:39:05 +0800

    added fixed register

--- main.py
+++ main.py
@@ -7,12 +7,14 @@ from datetime import datetime
 from routes import auth
 from auth import create_access_token, get_password_hash, verify_password, get_current_user
 from models import UserIn, UserOut, Token
-
-
+from auth import verify_password
+from models import User
+from sqlalchemy.orm import Session
+from database import get_db
 
 app = FastAPI()
 
-fake_users_db = {}
+
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
@@ -56,20 +58,27 @@ def get_data_history():
 
 
 @app.post("/register", response_model=UserOut)
-def register(user: UserIn):
-    if user.email in fake_users_db:
+def register(user: UserIn, db: Session = Depends(get_db)):
+    existing_user = db.query(User).filter(User.email == user.email).first()
+    if existing_user:
         raise HTTPException(status_code=400, detail="Email already registered")
+
     hashed_pw = get_password_hash(user.password)
-    fake_users_db[user.email] = {"email": user.email, "hashed_password": hashed_pw}
-    return {"email": user.email}
+    new_user = User(email=user.email, hashed_password=hashed_pw)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return {"email": new_user.email}
 
 @app.post("/token", response_model=Token)
-def login(form_data: OAuth2PasswordRequestForm = Depends()):
-    user = fake_users_db.get(form_data.username)  # username is actually email
-    if not user or not verify_password(form_data.password, user["hashed_password"]):
+def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.email == form_data.username).first()
+    if not user or not verify_password(form_data.password, user.hashed_password):
         raise HTTPException(status_code=401, detail="Invalid credentials")
-    token = create_access_token(data={"sub": form_data.username})
-    return {"access_token": token, "token_type": "bearer"}
+
+    access_token = create_access_token(data={"sub": str(user.id)})
+    return {"access_token": access_token, "token_type": "bearer"}
 
 
 @app.get("/protected")
