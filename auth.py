f8a54248cc1bc055d47f2625ea299d00b5a031f0
Gwapobenjie <plazabenjie1@gmail.com>
2025-05-16 03:11:02 +0800

    added get user

--- auth.py
+++ auth.py
@@ -1,18 +1,19 @@
 from passlib.context import CryptContext
 from datetime import datetime, timedelta
-from jose import jwt
+from jose import JWTError, jwt
 import os
 from dotenv import load_dotenv
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
 
 load_dotenv()
 
-SECRET_KEY = os.getenv("HGdRdcN6PhT4ZN3GjMtTsvqdVDvNLlttk681xwKk2aw")
-
-SECRET_KEY = "HGdRdcN6PhT4ZN3GjMtTsvqdVDvNLlttk681xwKk2aw"  # Use a secure random key!
+SECRET_KEY = os.getenv("SECRET_KEY", "fallback-hardcoded-secret-if-env-is-missing")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 def get_password_hash(password):
     return pwd_context.hash(password)
@@ -26,3 +27,19 @@ def create_access_token(data: dict):
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        user_id: str = payload.get("sub")
+        if user_id is None:
+            raise credentials_exception
+        return {"user_id": user_id}
+    except JWTError:
+        raise credentials_exception
